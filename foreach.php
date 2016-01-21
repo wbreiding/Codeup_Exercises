e95996a0a56f522e04fa003960a2c4c0cf67cd62
wbreiding <wdibean@gmail.com>
2016-01-21 15:20:15 -0800

    foreach part 3

--- foreach.php
+++ foreach.php
@@ -2,7 +2,13 @@
 $things = array('Sgt. Pepper', "11", null, array(1,2,3), 3.14, "12 + 7", false, (string) 11);
 
 foreach ($things as $thing) {
-  if (is_scalar($thing)) {
+  if (is_array($thing)) {
+    echo "Array (";
+    foreach ($thing as $value) {
+      echo "{$value},";
+    }
+    echo ")\n";
+  } else {
     echo "{$thing}\n";
   }
 }
