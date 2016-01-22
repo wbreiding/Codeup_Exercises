b73a6b74cfb3c484c1987458abbf7595a09f2987
wbreiding <wdibean@gmail.com>
2016-01-21 16:03:25 -0800

    functions part 1

--- arithmetic.php
+++ arithmetic.php
@@ -8,16 +8,34 @@ function add($a, $b)
 function subtract($a, $b)
 {
     // Add code here
+    return $a - $b;
 }
 
 function multiply($a, $b)
 {
     // Add code here
+    return $a * $b;
 }
 
 function divide($a, $b)
 {
     // Add code here
+    return $a/$b;
 }
 
 // Add code to test your functions here
+$a = 10;
+$b = 5;
+echo "Addition: ";
+echo add($a,$b);
+echo PHP_EOL;
+echo "Subtraction: ";
+echo subtract($a,$b);
+echo PHP_EOL;
+echo "Multiplication: ";
+echo multiply($a,$b);
+echo PHP_EOL;
+echo "Division: ";
+echo divide($a,$b);
+echo PHP_EOL;
+?>
