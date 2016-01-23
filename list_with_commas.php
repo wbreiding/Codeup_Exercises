60f1129a46ec7f5c2e2dfd79db4c9ad7b1ef11dc
wbreiding <wdibean@gmail.com>
2016-01-22 16:51:52 -0800

    explode, part4

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,7 +1,9 @@
 <?php
 // Converts array into list n1, n2, ..., and n3
-function humanizedList($array) {
-  sort($array);
+function humanizedList($array, $sort = FALSE) {
+  if ($sort) {
+    sort($array);
+  }
   $last = array_pop($array);
   $lastArray = explode(' ',$last);
   array_unshift($lastArray, 'and');
@@ -13,7 +15,7 @@ function humanizedList($array) {
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
 $physicistsArray = explode(', ',$physicistsString);
 // Humanize that list
- $famousFakePhysicists = humanizedList($physicistsArray);
+ $famousFakePhysicists = humanizedList($physicistsArray, TRUE);
 
 
 echo "Some of the most famous fictional theoretical physicists are {$famousFakePhysicists}.";
