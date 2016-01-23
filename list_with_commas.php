b68c7813ff0df60e3e477cea25a2520660c4d04e
wbreiding <wdibean@gmail.com>
2016-01-22 16:48:54 -0800

    explode, part2

--- list_with_commas.php
+++ list_with_commas.php
@@ -1,18 +1,17 @@
 <?php
 // Converts array into list n1, n2, ..., and n3
 function humanizedList($array) {
+  sort($array);
   $last = array_pop($array);
   $lastArray = explode(' ',$last);
-  array_shift($lastArray);
-  array_unshift($lastArray, ' and');
+  array_unshift($lastArray, 'and');
   $last = implode(' ', $lastArray);
   array_push($array, $last);
-  return implode(',', $array);
-
+  return implode(', ', $array);
 }
 
 $physicistsString = 'Gordon Freeman, Samantha Carter, Sheldon Cooper, Quinn Mallory, Bruce Banner, Tony Stark';
-$physicistsArray = explode(',',$physicistsString);
+$physicistsArray = explode(', ',$physicistsString);
 // Humanize that list
  $famousFakePhysicists = humanizedList($physicistsArray);
 
